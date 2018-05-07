9cff3a71ffe695920ce07c6c163a52bdf646e2fd
Edward Bryan Abergas <edwardbryan.abergas@gmail.com>
2018-05-07 15:49:26 +0800

    Initial commit.

---
+++ app/src/main/java/io/github/bry1337/todoscky/ui/addedittask/AddEditTaskActivity.kt
@@ -0,0 +1,11 @@
+package io.github.bry1337.todoscky.ui.addedittask
+
+import dagger.android.support.DaggerAppCompatActivity
+
+/**
+ * Created by bry1337 on 07/05/2018.
+ *
+ * @author edwardbryan.abergas@gmail.com
+ */
+class AddEditTaskActivity : DaggerAppCompatActivity() {
+}
